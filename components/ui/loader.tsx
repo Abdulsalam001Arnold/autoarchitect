c10b51099b270ee7054cd85cb7422d3ebd6503f1
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2026-03-08 23:09:26 +0100

    fix: styled components error fixed

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,73 +1,16 @@
-
 import React from "react";
-import styled from 'styled-components';
+import styles from "./loader.module.css";
 
 const Loader = () => {
   return (
-    <StyledWrapper>
-      <div className="loading">
-        <span />
-        <span />
-        <span />
-        <span />
-        <span />
-      </div>
-    </StyledWrapper>
+    <div className={styles.wrapper}>
+      <span className={`${styles.item} ${styles.item1}`} />
+      <span className={`${styles.item} ${styles.item2}`} />
+      <span className={`${styles.item} ${styles.item3}`} />
+      <span className={`${styles.item} ${styles.item4}`} />
+      <span className={`${styles.item} ${styles.item5}`} />
+    </div>
   );
-}
-
-const StyledWrapper = styled.div`
-  .loading {
-   --speed-of-animation: 0.9s;
-   --gap: 6px;
-   --first-color: #4c86f9;
-   --second-color: #49a84c;
-   --third-color: #f6bb02;
-   --fourth-color: #f6bb02;
-   --fifth-color: #2196f3;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   width: 100px;
-   gap: 6px;
-   height: 100px;
-  }
-
-  .loading span {
-   width: 4px;
-   height: 50px;
-   background: var(--first-color);
-   animation: scale var(--speed-of-animation) ease-in-out infinite;
-  }
-
-  .loading span:nth-child(2) {
-   background: var(--second-color);
-   animation-delay: -0.8s;
-  }
-
-  .loading span:nth-child(3) {
-   background: var(--third-color);
-   animation-delay: -0.7s;
-  }
-
-  .loading span:nth-child(4) {
-   background: var(--fourth-color);
-   animation-delay: -0.6s;
-  }
-
-  .loading span:nth-child(5) {
-   background: var(--fifth-color);
-   animation-delay: -0.5s;
-  }
-
-  @keyframes scale {
-   0%, 40%, 100% {
-    transform: scaleY(0.05);
-   }
-
-   20% {
-    transform: scaleY(1);
-   }
-  }`;
+};
 
 export default Loader;
